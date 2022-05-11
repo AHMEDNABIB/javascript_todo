6930841106fec2c1c4cd7b044720060da02c1b55
AHMED NABIB <nabibpallab@gmail.com>
2022-05-11 13:16:40 +0600

    todo ui complete

---
+++ conceptual/project.js
@@ -0,0 +1,7 @@
+document.getElementById('add-btn').addEventListener('click', function (e) {
+    const inputValue = document.getElementById("input-value").ariaValueMax
+
+    if (inputValue === '') {
+        
+    }
+})
