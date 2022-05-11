eeddad7ec1622be6136587e313a28c224af9c2ad
AHMED NABIB <nabibpallab@gmail.com>
2022-05-11 16:53:08 +0600

    delete and done button complete

--- conceptual/project.js
+++ conceptual/project.js
@@ -1,7 +1,7 @@
-let count =0
+let count = 0;
 
 document.getElementById("add-btn").addEventListener("click", function (e) {
-     count++
+	count++;
 
 	const inputValue = document.getElementById("input-value");
 
@@ -12,11 +12,43 @@ document.getElementById("add-btn").addEventListener("click", function (e) {
 		const tableContent = document.createElement("tr");
 		tableContent.innerHTML = `<th scope="row">${count}</th>
 							<td>${inputValue.value}</td>
-							<td>Otto</td>`;
+							<td><button class='btn btn-danger delete-btn'>Delete</button>
+                            <button class="btn btn-success done-btn">Done</button></td>`;
+
+		mainContainer.appendChild(tableContent);
         
-        mainContainer.appendChild(tableContent);
 
-        document.getElementById("input-value").value ='';
+        
+        document.getElementById("input-value").value = "";
+        
+        // if (e.target.value == "") {
+        //     document.getElementById("add-btn").setAttribute("disabled", true);
+			
+		// } else {
+		// 	document.getElementById("add-btn").removeAttribute("disabled");
+		// }
 
+		const deleteButton = document.getElementsByClassName("delete-btn");
+		const doneButton = document.getElementsByClassName("done-btn");
+
+		
+		for (const button of deleteButton) {
+			button.addEventListener("click", function (e) {
+				e.target.parentNode.parentNode.style.display = "none";
+			
+            });
+            
+
+          
+
+            for (const button of doneButton) {
+				button.addEventListener("click", function (e) {
+					e.target.parentNode.parentNode.style.textDecoration =
+						"line-through";
+                   
+				});
+			}
+                
+		}
 	}
 });
